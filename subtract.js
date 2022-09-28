500ff5f0394144d53404321e242bc3e961972a57
Saipranavi Ponnathota <pranavisau@gmail.com>
2022-09-28 14:54:30 +0530

    subtract logic created

---
+++ subtract.js
@@ -0,0 +1,9 @@
+function subtract()
+{
+
+
+var num1=document.getElementById("firstno").value
+var num2=document.getElementById("secondno").value
+var result=parseInt(num1)-parseInt(num2)
+document.getElementById("result").value=result
+}
